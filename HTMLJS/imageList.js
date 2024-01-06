797887754e7b2a9866cfb2897d4228aef3e03fb5
mebius4928143 <mebius_4928143@yahoo.co.jp>
2024-01-06 13:45:29 +0900

    1:45

--- HTMLJS/imageList.js
+++ HTMLJS/imageList.js
@@ -1,2 +1,12 @@
 $(function(){
+	for(var i=0;i<4;i++)
+	{
+		$(`#aaa tbody tr:nth(${i}) td:nth(0)`).on('click',(e)=>{
+			alert($('#aaa tbody tr').index(e.target.parentNode));
+			const ti= $('#aaa tbody tr').index(e.target.parentNode);
+			$('#list').appendTo(`#aaa tbody tr:nth(${ti}) td:nth(0)`);
+			$('#list').css('display','block');
+			$('#list').css('z-index','100');
+		});
+	}
 });
